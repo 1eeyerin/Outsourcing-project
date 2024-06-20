8454eeca0dc707f4bd10943802ce588df67f384e
Yerin <dev.yerin@gmail.com>
2024-06-21 07:49:54 +0900

    fix: 안쓰는 라이브러리 제거 - svgr

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from '@svgr/rollup';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src/') }]
   }
