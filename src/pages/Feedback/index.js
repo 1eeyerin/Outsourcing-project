ba4a2a77fb5dfdad46bc5efd37d895e9c48d589b
Yerin <dev.yerin@gmail.com>
2024-06-21 22:21:35 +0900

    feat: 코드리뷰를 위한 작업 영역 revert

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -1,4 +1,1 @@
-export { default as FeedbackForm } from './FeedbackForm';
-export { default as PasswordCheck } from './PasswordCheck';
-export { default as FeedbackDetail } from './FeedbackDetail';
 export { default } from './Feedback';
--- src/supabase/request.js
+++
@@ -1,9 +0,0 @@
-export const handleSupabaseRequest = async (request) => {
-  const { data, error } = await request;
-
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  return data;
-};
