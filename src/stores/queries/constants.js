e0bd4d91fb90b51efa7b990d548013c5dee5d022
Yerin <dev.yerin@gmail.com>
2024-06-22 13:23:42 +0900

    feat: constants 추가

--- src/stores/queries/constants.js
+++
@@ -1,7 +0,0 @@
-export const QUERY_KEYS = {
-  FEEDBACKS: ['feedbacks'],
-  FEEDBACK: (id) => ['feedback', id],
-  FEEDBACK_PASSWORD_CHECK: (id) => ['feedback-password', id],
-  MENUS: (category) => ['menus', category],
-  FETCH_LIMITED_MENUS: (count) => ['fetchLimitedMenus', count]
-};
